d5169822a98ddb607887cbbef1ec6f26291a5ecb
condortudorandrei <condortudorandrei@gmail.com>
2026-05-19 20:54:40 +0300

    Added IF statements and a few FOR structures

--- 03_flow.py
+++ 03_flow.py
@@ -15,7 +15,29 @@ for elem in range(5):
 print("Nr executii:")
 print(nr_exe)
 
+#IF statements:
+populatie_cluj = 300000
+if populatie_cluj > 250000:
+    print("Prea Mare")
+else:
+    print("Mai incape")
 
+list2 = [6,7,10,90,100,33,88,5,13,0]
+nr_pare = []
+nr_impare = []
+
+for elem in list2:
+    if elem % 2 == 0:
+        nr_pare.append(elem)
+    else:
+        nr_impare.append(elem)
+
+print(nr_pare)
+print(nr_impare)
+
+for elem in list2:
+    if elem % 2 == 0 and elem % 5 == 0:
+        print(elem)
 
 
 
