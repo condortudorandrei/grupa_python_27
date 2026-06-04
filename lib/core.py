82c516457dd9159b16acdc2322cb62a007e3edbc
condortudorandrei <condortudorandrei@gmail.com>
2026-06-04 20:53:16 +0300

    Added random chars function

--- lib/core.py
+++ lib/core.py
@@ -1,4 +1,4 @@
-
+import random
 
 def even_numbers(list):
     phlist = []
@@ -22,7 +22,11 @@ def is_even(nr):
     return nr % 2 == 0
 
 
-
+def generate_random_chars(min_txt = 97, max_txt = 122, count = 10):
+    random_letters = []
+    step1 = random.sample(range(min_txt, max_txt + 1), count)
+    random_letters = list(map(lambda x: chr(x), step1))
+    return(random_letters)
 
 
 
